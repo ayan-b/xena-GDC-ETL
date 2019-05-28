81ff941a86add38a8826a7f9921498e6aac81b2d
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-05-29 00:18:52 +0530

    Add script which removes redundant rows

--- xena_gdc_etl/scripts/remove_rows.py
+++ xena_gdc_etl/scripts/remove_rows.py
@@ -1,38 +1,36 @@
 import pandas as pd
 
 
-# rows represent IDs
-xena_dtype_row_ID = [
-    "htseq_counts",
-    "htseq_fpkm-uq",
-    "htseq_fpkm",
-    "mirna",
-    "methylation450",
-]
+def remove_unused_rows(cohorts, path_to_data="./"):
+    """This function removes redundant samples (rows) in phenotype data.
 
-# corresponding column represents IDs
-xena_dtype_col_ID = {
-    "masked_cnv": "sample",
-    "muse_snv": "Sample_ID",
-    "mutect2_snv": "Sample_ID",
-    "somaticsniper_snv": "Sample_ID",
-    "varscan2_snv": "Sample_ID",
-}
-
-# specify the cohorts here
-cohorts = [
-    "TCGA-CHOL",
-]
-
-dest = "GDC_phenotype"
-
-# path to the project data
-PATH = "./"
-
-
-def remove_unused_rows():
+    Args:
+        cohorts (str or list): A list of cohorts for which the unused rows
+            will be removed.
+        path_to_data (str, optional): Path to the data stored. If nothing is
+            passed the current directly will be considered.
+    """
+    dest = "GDC_phenotype"
+    # rows represent IDs
+    xena_dtype_row_ID = [
+        "htseq_counts",
+        "htseq_fpkm-uq",
+        "htseq_fpkm",
+        "mirna",
+        "methylation450",
+    ]
+    # corresponding column represents IDs
+    xena_dtype_col_ID = {
+        "masked_cnv": "sample",
+        "muse_snv": "Sample_ID",
+        "mutect2_snv": "Sample_ID",
+        "somaticsniper_snv": "Sample_ID",
+        "varscan2_snv": "Sample_ID",
+    }
+    if isinstance(cohorts, str):
+        cohorts = [cohorts]
     for cohort in cohorts:
-        matrix_path = PATH + cohort + "/Xena_Matrices/" + cohort + "."
+        matrix_path = path_to_data + cohort + "/Xena_Matrices/" + cohort + "."
         rows = []
         for dtype in xena_dtype_row_ID:
             df = pd.read_csv(matrix_path + dtype + ".tsv", sep="\t")
@@ -49,7 +47,3 @@ def remove_unused_rows():
         dest_df = dest_df.set_index("submitter_id.samples")
         dest_df = dest_df.drop(to_remove, axis=0)
         dest_df.to_csv(matrix_path + dest + ".tsv", sep="\t")
-
-
-if __name__ == "__main__":
-    remove_unused_rows()
